fb0919581b7bda0bb63c63d95ca307e6cab821bf
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-01 11:48:31 +0100

    Button is changed with href a anchor tag

--- react-mui-demo/src/components/MuiButtons.tsx
+++ react-mui-demo/src/components/MuiButtons.tsx
@@ -4,7 +4,7 @@ export const MuiButtons = () => {
   return (
     <div>
       <Stack spacing={2} direction='row'>
-        <Button variant="text">Text</Button>
+        <Button variant="text" href='https://google.com'>Text</Button>
         <Button variant="contained">Contained</Button>
         <Button variant="outlined">Outlined</Button>
       </Stack>
