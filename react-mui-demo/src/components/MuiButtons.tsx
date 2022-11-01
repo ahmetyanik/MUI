5d74d1428b1b51575f2cce7d7a3cee70bd326f2b
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-01 11:44:55 +0100

    3 variants of Button created and Stack composed

---
+++ react-mui-demo/src/components/MuiButtons.tsx
@@ -0,0 +1,13 @@
+import { Stack, Button } from "@mui/material";
+
+export const MuiButtons = () => {
+  return (
+    <div>
+      <Stack spacing={2}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  );
+};
