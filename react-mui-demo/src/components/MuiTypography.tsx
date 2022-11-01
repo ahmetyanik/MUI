07d99319cbf02e2724abfa599079b7b3937bbcc4
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-01 11:25:52 +0100

    variant added

---
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Typography} from '@mui/material'
+
+function MuiTypography() {
+  return (
+    <div>
+        <Typography variant='h1'>h1 heading</Typography>
+        <Typography variant='h2'>h2 heading</Typography>
+        <Typography variant='h3'>h3 heading</Typography>
+        <Typography variant='h4'>h4 heading</Typography>
+        <Typography variant='h5'>h5 heading</Typography>
+        <Typography variant='h6'>h6 heading</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
