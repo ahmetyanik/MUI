c09ddf575fd623452a12ca7db2bf08d6e3478e99
ahmetyanik <ahmetyank4242@gmail.com>
2022-11-03 19:28:03 +0100

    Radio Button

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css';
-import { MuiButtons } from './components/MuiButtons';
-import { MuiSelect } from './components/MuiSelect';
-import { MuiTextField } from './components/MuiTextField';
+import "./App.css";
+import { MuiButtons } from "./components/MuiButtons";
+import { MuiRadioButton } from "./components/MuiRadioButton";
+import { MuiSelect } from "./components/MuiSelect";
+import { MuiTextField } from "./components/MuiTextField";
 // import MuiTypography from './components/MuiTypography';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       {/* <MuiTypography/> */}
       {/* <MuiButtons/> */}
       {/* <MuiTextField/> */}
-      <MuiSelect/>
+      {/* <MuiSelect/> */}
+      <MuiRadioButton />
     </div>
   );
 }
